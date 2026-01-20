97c7cc5636a6bce92869c3fa85b6c5da890ca068
Meyggy <mmeyggy@gmail.com>
2026-01-20 22:57:25 +0700

    lg

--- src/artikel.jsx
+++ src/artikel.jsx
@@ -21,9 +21,9 @@ function Artikel() {
             <div className="bar"></div>
           </div>
           <ul className={`nav-links ${menuAktif ? "active" : ""}`}>
-            <Link to="/" className="btnKembali">
+            <li><Link to="/" className="btnKembali">
           Beranda
-        </Link>
+        </Link></li>
             <li><Link to="/artikel">Artikel</Link></li>
             <li><a href="about.html">About Us</a></li>
 						<li><a href="index.html#kontak">Kontak</a></li>
