4f22d37e15b72026e42e862bd64af8bf8a7b7831
Irma Tiara <irmatiara598@gmail.com>
2026-01-25 12:57:29 +0700

    perbaikan navbar untuk detail artikel

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -4,25 +4,31 @@ import { useState } from 'react'
 function Navbar() {
 	const [menuOpen, setMenuOpen] = useState(false);
 	const location = useLocation();
+	const detailPagePaths = ['/ai', '/html', '/internet', '/algo', '/notfound'];
+	const isDetailPage = detailPagePaths.includes(location.pathname);
 
 	return (
 		<nav className="navbar">
 			<div className="container">
 				<img className="logo" src="/assets/logo.svg" alt="" />
-				<div
-					className={`burger-menu ${menuOpen ? "toggle" : ""}`}
-					onClick={() => setMenuOpen(!menuOpen)}
-				>
-					<div className="bar" />
-					<div className="bar" />
-					<div className="bar" />
-				</div>
-				<ul className={`nav-links ${menuOpen ? "active" : ""}`}>
-					<li><Link to="/" className={location.pathname === '/' ? 'active' : ''}>Beranda</Link></li>
-					<li><Link to="/artikel" className={location.pathname === '/artikel' ? 'active' : ''}>Artikel</Link></li>
-					<li><Link to="/about" className={location.pathname === '/about' ? 'active' : ''}>About Us</Link></li>
-					<li><Link to="/#kontak" className={location.hash === '#kontak' ? 'active' : ''}>Kontak</Link></li>
-				</ul>
+				{!isDetailPage && (
+					<>
+						<div
+							className={`burger-menu ${menuOpen ? "toggle" : ""}`}
+							onClick={() => setMenuOpen(!menuOpen)}
+						>
+							<div className="bar" />
+							<div className="bar" />
+							<div className="bar" />
+						</div>
+						<ul className={`nav-links ${menuOpen ? "active" : ""}`}>
+							<li><Link to="/" className={location.pathname === '/' ? 'active' : ''}>Beranda</Link></li>
+							<li><Link to="/artikel" className={location.pathname === '/artikel' ? 'active' : ''}>Artikel</Link></li>
+							<li><Link to="/about" className={location.pathname === '/about' ? 'active' : ''}>About Us</Link></li>
+							<li><Link to="/#kontak" className={location.hash === '#kontak' ? 'active' : ''}>Kontak</Link></li>
+						</ul>
+					</>
+				)}
 			</div>
 		</nav>
 	)
