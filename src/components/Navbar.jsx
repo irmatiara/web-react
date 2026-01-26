a46c0633eacc317d9576164d4a16c4ffc290c8b6
Irma Tiara <irmatiara598@gmail.com>
2026-01-26 22:29:56 +0700

    penambahan menu kembali pada halaman notFound

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -2,10 +2,13 @@ import { Link, useLocation } from 'react-router-dom'
 import { useState } from 'react'
 
 function Navbar() {
-	const [menuOpen, setMenuOpen] = useState(false);
-	const location = useLocation();
-	const detailPagePaths = ['/ai', '/html', '/internet', '/algo', '/notfound'];
-	const isDetailPage = detailPagePaths.includes(location.pathname);
+	const [menuOpen, setMenuOpen] = useState(false)
+	const location = useLocation()
+	const isNotFound = location.pathname === '/notfound'
+	const detailPagePaths = ['/ai', '/html', '/internet', '/algo']
+	const isDetailPage = detailPagePaths.some(path =>
+		location.pathname.startsWith(path)
+	)
 
 	return (
 		<nav className="navbar">
@@ -13,19 +16,23 @@ function Navbar() {
 				<img className="logo" src="/assets/logo.svg" alt="" />
 				{!isDetailPage && (
 					<>
-						<div
-							className={`burger-menu ${menuOpen ? "toggle" : ""}`}
-							onClick={() => setMenuOpen(!menuOpen)}
-						>
+						<div className={`burger-menu ${menuOpen ? 'toggle' : ''}`} onClick={() => setMenuOpen(!menuOpen)}>
 							<div className="bar" />
 							<div className="bar" />
 							<div className="bar" />
 						</div>
-						<ul className={`nav-links ${menuOpen ? "active" : ""}`}>
-							<li><Link to="/" className={location.pathname === '/' ? 'active' : ''}>Beranda</Link></li>
-							<li><Link to="/artikel" className={location.pathname === '/artikel' ? 'active' : ''}>Artikel</Link></li>
-							<li><Link to="/about" className={location.pathname === '/about' ? 'active' : ''}>About Us</Link></li>
-							<li><Link to="/#kontak" className={location.hash === '#kontak' ? 'active' : ''}>Kontak</Link></li>
+
+						<ul className={`nav-links ${menuOpen ? 'active' : ''}`}>
+							{isNotFound ? (
+								<li><Link to="/artikel" className={location.pathname === '/artikel' ? 'active' : ''}>Artikel</Link></li>
+							) : (
+								<>
+									<li><Link to="/" className={location.pathname === '/' ? 'active' : ''}>Beranda</Link></li>
+									<li><Link to="/artikel" className={location.pathname === '/artikel' ? 'active' : ''}>Artikel</Link></li>
+									<li><Link to="/about" className={location.pathname === '/about' ? 'active' : ''}>About Us</Link></li>
+									<li><Link to="/#kontak" className={location.hash === '#kontak' ? 'active' : ''}>Kontak</Link></li>
+								</>
+							)}
 						</ul>
 					</>
 				)}
